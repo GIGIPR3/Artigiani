569e14aee353f83b5044fc180d846c3168adec15
luigi <luigidev6464@gmail.com>
2023-09-01 14:54:34 +0200

    homepage, login, register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegistratiComponent } from './registrati/registrati.component';
+import { LoginComponent } from './login/login.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomepageComponent},
+
+  {
+    path:'registrati', component:RegistratiComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  },
+  {
+    path:'marketplace', component: MarketplaceComponent
+  },
+  {
+    path:'admin', component: AdminComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegistratiComponent } from './registrati/registrati.component';
+import { LoginComponent } from './login/login.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AdminComponent } from './admin/admin.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    RegistratiComponent,
+    LoginComponent,
+    MarketplaceComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
