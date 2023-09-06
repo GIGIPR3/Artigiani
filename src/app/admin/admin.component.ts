d5f20dd00200418e7d972c00a785f8c5050f7a1f
luigi <luigidev6464@gmail.com>
2023-09-06 17:26:11 +0200

    .

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -17,19 +17,6 @@ export class AdminComponent {
 
   constructor(private http: LoggedUserService) {this.http.getUser()}
 
-  /* modaleVisible = false;
-
-  myModale: HTMLCollectionOf<Element> = document.getElementsByClassName("form");
-
-  visualizzaModale(){
-    addEventListener("click", () => {
-      this.modaleVisible = !this.modaleVisible
-      if (this.modaleVisible) {
-        this.myModale.classList.remove("disabilita")
-        this.myModale.classList.add("attiva")
-      }
-    })
-  } */
 
   modaleVisibile = false;
 
