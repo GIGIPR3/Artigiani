6c10357c0976e60140eb4e68230bcd3467e3aa10
luigi <luigidev6464@gmail.com>
2023-09-07 11:18:02 +0200

    .

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 /* import { ServizioService } from '../service/servizio.service'; */
 import { User } from '../interfaces/user';
 import { LoggedUserService } from '../service/logged-user.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+
+
+  productForm = FormGroup
 
   user: User | null = this.http.user;
 
   constructor(private http: LoggedUserService) {this.http.getUser()}
+
+  ngOnInit(): void {
+
+    this.productForm = new FormGroup ({
+        /* img: new FormControl(null), */
+        titolo: new FormControl(null),
+        prezzo: new FormControl(null)
+    })
+  }
 
 
   modaleVisibile = false;
@@ -21,5 +34,13 @@ export class AdminComponent {
     this.modaleVisibile = !this.modaleVisibile
 
   }
+
+
+
+
+
+
+
+
 
 }
---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,5 @@
+export interface Product {
+  img: string,
+  titolo: string,
+  prezzo: number
+}
