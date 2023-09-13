8967bdcd4143ec8dc0a1b0aea050c8b4cfcaff70
MicheleCe <michele.cerqua@hotmail.com>
2023-09-13 17:14:05 +0200

    cartService

--- src/app/carrello/carrello.component.ts
+++ src/app/carrello/carrello.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../service/cart.service';
 import { LoggedUserService } from '../service/logged-user.service';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-carrello',
   templateUrl: './carrello.component.html',
   styleUrls: ['./carrello.component.css'],
 })
-export class CarrelloComponent {
+export class CarrelloComponent implements OnInit {
   cartItems: any[] | null = [];
 
-  constructor(private loggedUserService: LoggedUserService) {}
+  constructor(
+    private loggedUserService: LoggedUserService,
+    private cartService: CartService,
+    private productService: ProductService
+  ) {}
+
+  cart: string[] = [];
+  productInCart: any[] = [];
+
+  ngOnInit(): void {
+    const cartJson = this.cartService.getCart();
+
+    if (cartJson) {
+      this.cart = JSON.parse(cartJson);
+      console.log('User data:', this.cart);
+    } else {
+      console.log('User data not found in cookies');
+    }
+    this.cart.forEach((cartItem) =>
+      this.productService
+        .getProductbyId(cartItem)
+        .subscribe((response) => this.productInCart.push(response))
+    );
+    console.log(this.productInCart);
+  }
 }
--- src/app/service/logged-user.service.ts
+++ src/app/service/logged-user.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { User } from '../interfaces/user';
+import { CartService } from './cart.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoggedUserService {
-  constructor(private cookieService: CookieService, private router: Router) {}
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+    private cartService: CartService
+  ) {}
 
   user: User | null = null;
 
@@ -24,5 +29,6 @@ export class LoggedUserService {
     this.cookieService.deleteAll();
     this.user = null;
     this.router.navigate(['/login']);
+    this.cartService.clearCart();
   }
 }
