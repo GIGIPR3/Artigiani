dfa7fa18857fee3beca7fabb6c309baf346a9ef1
MicheleCe <michele.cerqua@hotmail.com>
2023-09-13 12:37:57 +0200

    added review and rating

--- src/app/carrello/carrello.component.ts
+++ src/app/carrello/carrello.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { LoggedUserService } from '../service/logged-user.service';
 
 @Component({
   selector: 'app-carrello',
   templateUrl: './carrello.component.html',
-  styleUrls: ['./carrello.component.css']
+  styleUrls: ['./carrello.component.css'],
 })
 export class CarrelloComponent {
+  cartItems: any[] | null = [];
 
+  constructor(private loggedUserService: LoggedUserService) {}
 }
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoggedUserService } from '../service/logged-user.service';
 import { ProductService } from '../service/product.service';
+import { ReviewService } from '../service/review.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,7 +13,33 @@ import { ProductService } from '../service/product.service';
 export class ProductCardComponent {
   @Input() productData: any;
 
-  constructor(private router: Router, private productService: ProductService) {}
+  descriptionForm: FormGroup;
+  reviews: any[] = [];
+  showReview: boolean = false;
+
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private loggedUser: LoggedUserService,
+    private formBuilder: FormBuilder,
+    private reviewService: ReviewService
+  ) {
+    const userJson = JSON.parse(this.loggedUser.getUser());
+    console.log(userJson);
+    this.descriptionForm = this.formBuilder.group({
+      userId: [userJson.userId],
+      productId: [''],
+      comment: [''],
+      rating: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(/^[0-9]*$/),
+          Validators.max(10),
+        ],
+      ],
+    });
+  }
 
   selectedImageIndex: number | null = null;
 
@@ -25,5 +54,33 @@ export class ProductCardComponent {
   onDelete(productId: string) {
     this.productService.deleteProduct(productId).subscribe();
     window.location.reload();
+  }
+
+  addDescription(productId: string) {
+    const description = this.descriptionForm.get('comment')!.value;
+    const ratingString = this.descriptionForm.get('rating')!.value;
+
+    const rating = parseInt(ratingString, 10);
+
+    if (isNaN(rating) || rating < 0 || rating > 10) {
+      console.error('Rating is not a valid integer between 0 and 10');
+      return;
+    }
+
+    this.descriptionForm.get('productId')!.setValue(productId);
+
+    console.log('ProductId:', productId);
+    console.log('Description:', description);
+    console.log('Rating:', rating);
+    console.log('Form Value:', this.descriptionForm.value);
+    this.reviewService.postReview(this.descriptionForm.value).subscribe();
+  }
+
+  getReviews(productId: string) {
+    this.showReview = !this.showReview;
+    this.reviewService
+      .getReviewsFromProduct(productId)
+      .subscribe((reviews) => (this.reviews = reviews));
+    console.log(this.reviews);
   }
 }
